f60cddbfeb490da11fa6f520aa4aa66ffe2ff99c
ShpetimV <shpetimveseli77@gmail.com>
2025-05-14 10:46:34 +0200

    removed logoticker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
         <>
             <Header/>
             <Hero/>
-            <LogoTicker/>
             <ProductShowcase/>
             <Pricing/>
             <Testimonials/>
